52c706c981792f0c723d8980051cdabe3bb3d5c4
Falak Ansari <ansarifalak105@gmail.com>
2025-09-04 11:53:15 +0530

    generative_ai , setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Falak Ansari',
+    author_email= 'ansarifalak105@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
